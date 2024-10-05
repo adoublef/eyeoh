abee8e80a148d50cb782e7383eb85b3f5e215f4a
Rahim Afful-Brown <kristopherab@gmail.com>
2024-10-05 15:56:12 +0100

    modify interface to return mime

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -9,7 +9,7 @@ import (
 
 type Uploader interface {
 	Upload(ctx context.Context, r io.Reader) (id uuid.UUID, sz int64, err error)
-	Download(ctx context.Context, id uuid.UUID) (io.ReadCloser, error)
+	Download(ctx context.Context, id uuid.UUID) (rc io.ReadCloser, mime string, err error)
 }
 
 type FS struct {
