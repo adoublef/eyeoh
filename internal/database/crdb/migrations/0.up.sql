9810d4a7483f3304bdf548c9697db5c90105e119
Rahim Afful-Brown <kristopherab@gmail.com>
2024-10-07 00:15:59 +0100

    cleanup

--- internal/database/crdb/migrations/0.up.sql
+++ internal/database/crdb/migrations/0.up.sql
@@ -4,12 +4,8 @@ create table fs.dir_entry (
   id uuid
   , root uuid
   , name text not null check (name <> '' and length(name) < 256)
-  -- if directory then this value is set as null
-  -- ref_id can only be set if sz is also set
-  -- , ref uuid
-  -- , sz int default 0 check (sz >= 0)
   , mod_at timestamptz default now()
-  -- mvcc
+  -- mvcc (i.e. renaming, moving or updating blob data)
   , v int default 0 check (v >= 0)
   , unique (coalesce(root, b'0000000000000000'), name)
   , foreign key (root) references fs.dir_entry (id)
@@ -24,7 +20,7 @@ create table fs.blob_data (
   -- hash is required
   , sha bytes not null
   , mod_at timestamptz default now()
-  -- this will be needed?
+  -- (dir_entry can refernce this for history)
   , v int
   , foreign key (dir_entry) references fs.dir_entry (id)
   , primary key (id)
